76e4dbd2d6e7feea1093eb475b98811981a1b7e1
Chris Hunt <mail@thisisthechris.co.uk>
2015-07-22 14:00:39 +0100

    Fixes tools reference

--- qualia/config/settings/base.py
+++ qualia/config/settings/base.py
@@ -39,7 +39,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     "django.core.context_processors.static",
     "django.core.context_processors.tz",
     "django.contrib.messages.context_processors.messages",
-    "qualia.tools.context_processors.branding"
+    "tools.context_processors.branding"
 )
 
 ROOT_URLCONF = 'config.urls'
