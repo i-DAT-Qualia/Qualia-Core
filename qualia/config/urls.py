89069564d960be10ec0deeaf070c9942ee9836a7
Chris Hunt <mail@thisisthechris.co.uk>
2015-07-22 13:57:39 +0100

    Adds base URLs

---
+++ qualia/config/urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+from django.views.generic import RedirectView
+from django.conf import settings
+
+urlpatterns = [
+    # url(r'^$', 'dashboard.views.front_page'),
+    url(r'^admin/', include(admin.site.urls)),
+    # url(r'api/', include('config.api_urls')),
+
+    url(r'^accounts/', include('registration.backends.default.urls')),
+    # url(r'dashboard/', include('dashboard.urls')),
+
+    url(r'^favicon\.ico$', RedirectView.as_view(url=settings.STATIC_URL + settings.FAVICON)),
+]
